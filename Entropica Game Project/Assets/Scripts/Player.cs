b10743399081be2e58c5e72a04c5d7dde3a381e9
Md Muntasir Khan <md.muntasir.khan@gmail.com>
2025-07-29 14:47:36 -0400

    Jump physics properly implemented

--- Entropica Game Project/Assets/Scripts/Player.cs
+++ Entropica Game Project/Assets/Scripts/Player.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private bool jump;
+    private float horizontalInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +19,17 @@ public class Player : MonoBehaviour
         {
             // Log a message to the console.
             Debug.Log("Space key was pressed!");
+            jump = true;
+        }
+    }
 
+    // FixedUpdate is called at a fixed time interval, typically used for physics calculations.
+    void FixedUpdate()
+    {
+        if (jump)
+        {
             GetComponent<Rigidbody>().AddForce(Vector3.up * 5, ForceMode.Impulse);
-
+            jump = false; // Reset jump after applying force
         }
     }
 
