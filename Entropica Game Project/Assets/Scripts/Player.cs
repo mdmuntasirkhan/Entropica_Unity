d072ef3646a882fc0bd83454ae42d68cd19bc3c4
Md Muntasir Khan <md.muntasir.khan@gmail.com>
2025-07-29 13:11:13 -0400

    Jump script added

--- Entropica Game Project/Assets/Scripts/Player.cs
+++ Entropica Game Project/Assets/Scripts/Player.cs
@@ -2,5 +2,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if the space key is pressed down.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Log a message to the console.
+            Debug.Log("Space key was pressed!");
+
+            GetComponent<Rigidbody>().AddForce(Vector3.up * 5, ForceMode.Impulse);
+
+        }
+    }
+
 }
